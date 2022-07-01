93debc82f365e00592ec56a0257dedf7db39f2ec
edujca <edujca@gmail.com>
2022-07-01 19:32:21 +0200

    Día 2

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,11 @@
+//Un objeto agrupa todo en una sola variable
+
+//Object literal --> = y llaves
+const producto = {
+    nombre: "Monitor de 20 pulgadas", //llave o propiedad : valor (usa dos puntos en lugar de igual, y coma al final de cada llave)
+    precio: 300,
+    disponible: true //puede llevar coma o no
+
+}
+
+console.log(producto);
