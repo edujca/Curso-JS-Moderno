ae8ea5643a26bcd9af75f035b0a75229b2dc9cec
edujca <edujca@gmail.com>
2022-07-04 19:07:11 +0200

    4/7/22

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,21 @@
+//Las funciones son reutilizables y realizan una acción
+
+//Dos formas de crearlas: declaración y expresión
+
+//Declaración de Función (Function Declaration)
+
+function sumar() {
+    console.log( 2 + 2);
+}
+
+sumar();
+sumar();
+
+//Expresión de Función (Function Expression)
+
+const sumar2 = function() {
+    console.log(3+3);
+        
+}
+
+sumar2();
